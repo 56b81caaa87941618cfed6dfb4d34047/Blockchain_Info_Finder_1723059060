fcb11eb53fa69c3f43687efe1fad2fd9289e7f11
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-08 01:01:41 +0530

    Code Generation

--- components/simple_header_component_1723059063.js
+++ components/simple_header_component_1723059063.js
@@ -27,6 +27,7 @@ Vue.component("simple_header_component_1723059063", {
                         <li id="nav-item-1"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Trending cryptocurrencies</a></li>
                         <li id="nav-item-2"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Upcoming ICOs and token sales</a></li>
                         <li id="nav-item-3"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Blockchain technology news</a></li>
+                        <li id="nav-item-submit"><button class="px-4 py-2 font-bold text-white bg-purple-700 rounded hover:bg-purple-600 focus:outline-none focus:shadow-outline">Submit</button></li>
                     </ul>
                 </div>
             </div>
